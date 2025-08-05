0e6bf5e0ba377f1697332169580df78ed80b561e
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2025-08-05 12:47:39 +0530

    added C++ Basic Template

---
+++ Day1/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+    public static void main(String [] args)
+    {
+        System.out.println("Hello World");  
+        System.out.println("Text whatever you wish to print");
+        System.out.print(7+21);
+    }
+}
