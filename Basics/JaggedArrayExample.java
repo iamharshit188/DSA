caf82bd4111d222325ab400b667fd6ee11b59514
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2025-09-01 12:18:24 +0530

    added jagged array program

---
+++ Basics/JaggedArrayExample.java
@@ -0,0 +1,20 @@
+public class JaggedArrayExample {
+    public static void main(String [] args)
+    {
+        int [][] jag = new int [3][];
+        jag[0] = new int[] { 1 , 2};
+        jag[1] = new int[] { 3 , 4, 5};
+        jag[2] = new int[] { 6 };
+        
+        System.out.println("Jagged Array Example");
+        for(int i = 0; i< jag.length; i++)
+        {
+            for(int j = 0; j< jag[i].length; j++)
+            {
+                System.out.print(jag[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
