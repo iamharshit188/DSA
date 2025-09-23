2fda06bd841b3d681e78e9acfef764ebda5503ed
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2025-09-23 11:25:51 +0530

    Add Fibonacci sequence implementation in Basics/Fibonacci.java

---
+++ Basics/Fibonacci.java
@@ -0,0 +1,13 @@
+class Fibonacci{
+    public static void main(String[] args) {
+        int n = 10; // Number of terms in the Fibonacci sequence
+        int a = 0, b = 1;
+        System.out.print("Fibonacci Series: " + a + ", " + b);
+
+        for (int i = 2; i < n; i++) {
+            int next = a + b;
+            System.out.print(", " + next);
+            a = b;
+            b = next;
+        }
+    }
